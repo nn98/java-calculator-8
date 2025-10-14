5cccabbaa713b4696b54a534b24ab04ba43fa5d5
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-14 16:22:42 +0900

    feat(io): autoFlush 추가

--- src/main/java/io/IO.java
+++ src/main/java/io/IO.java
@@ -3,6 +3,7 @@ package io;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
 public class IO {
@@ -10,16 +11,16 @@ public class IO {
 	private final PrintWriter writer;
 	public IO() {
 		this.reader = new BufferedReader(new InputStreamReader(System.in));
-		this.writer = new PrintWriter(System.out);
+		this.writer = new PrintWriter(new OutputStreamWriter(System.out), true);
 	}
-	public String readLine() throws IllegalArgumentException, IOException {
+	public String readLine() throws IOException {
 		return this.reader.readLine();
 	}
-	public String readLine(String prompt) throws IllegalArgumentException, IOException {
-		this.writer.print(prompt);
+	public String readLine(String prompt) throws IOException {
+		this.writer.println(prompt);
 		return this.reader.readLine();
 	}
-	public void writeLine(String prompt) throws IllegalArgumentException {
-		this.writer.print(prompt);
+	public void writeLine(String prompt) {
+		this.writer.println(prompt);
 	}
 }
