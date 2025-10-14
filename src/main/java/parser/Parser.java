f741234bc2587f212610c0a620e7b1b8765ee841
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-14 17:26:19 +0900

    test(parser): TDD / 커스텀 구분자 유무, 계산식 추출 테스트 구현
    클래스 / 생성자 / 메소드가 있어야 에러는 안나고 실행되는구나
    
    스켈레톤? 스텁? 만 생성해서 테스트 실패 확인

---
+++ src/main/java/parser/Parser.java
@@ -0,0 +1,14 @@
+package parser;
+
+public class Parser {
+	public Parser(String s) {
+	}
+
+	public String getCustom() {
+		return "";
+	}
+
+	public String getExpression() {
+		return "";
+	}
+}
---
+++ src/test/java/parser/ParserTest.java
@@ -0,0 +1,31 @@
+package parser;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class ParserTest {
+	@Test
+	void 커스텀_구분자_추출_테스트() throws Exception {
+		Parser parser = new Parser("//;\n1;2;3");
+		assertThat(parser.getCustom()).isEqualTo(";");
+	}
+
+	@Test
+	void 커스텀_구분자_추출_테스트_null() throws Exception {
+		Parser parser = new Parser("/;\n1;2;3");
+		assertThat(parser.getCustom()).isNull();
+	}
+
+	@Test
+	void 계산식_추출_테스트() {
+		Parser parser = new Parser("//;\n1;2;3");
+		assertThat(parser.getExpression()).isEqualTo("1;2;3");
+	}
+
+	@Test
+	void 계산식_공백_테스트() throws Exception {
+		Parser parser = new Parser("");
+		assertThat(parser.getExpression()).isEqualTo("0");
+	}
+}
