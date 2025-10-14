accdf4ad86b8538b780aa718932fea2b993a4e22
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-14 16:29:01 +0900

    feat(io): io 주입 생성자 추가
    Strength : 테스트 용이, 결합도 감소.
    
    Weekness : 캡슐화 약화, 간접화 증가(오버헤드) - 학습 곡선?

--- src/main/java/io/IO.java
+++ src/main/java/io/IO.java
@@ -5,13 +5,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Writer;
 
 public class IO {
 	private final BufferedReader reader;
 	private final PrintWriter writer;
 	public IO() {
-		this.reader = new BufferedReader(new InputStreamReader(System.in));
-		this.writer = new PrintWriter(new OutputStreamWriter(System.out), true);
+		this(new InputStreamReader(System.in), new OutputStreamWriter(System.out));
+	}
+	public IO(Reader reader, Writer writer) {
+		this.reader = new BufferedReader(reader);
+		this.writer = new PrintWriter(writer, true);
 	}
 	public String readLine() throws IOException {
 		return this.reader.readLine();
