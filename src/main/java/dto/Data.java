d6833bf4326caed6a19013028fc3e323536d5711
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-19 08:45:59 +0900

    feat(dto): 컨벤션 적용, 테스트 및 디버깅용 toString 구현

--- src/main/java/dto/Data.java
+++ src/main/java/dto/Data.java
@@ -1,16 +1,29 @@
-﻿package dto;
+
+package dto;
+
+import java.util.Arrays;
 
 public class Data {
+
 	private final String[] numbers;
 	private final String[] separators;
+
 	public Data(String[] numbers, String[] separators) {
 		this.numbers = numbers;
 		this.separators = separators;
 	}
+
 	public String[] getNumbers() {
 		return numbers;
 	}
+
 	public String[] getSeparators() {
 		return separators;
 	}
+
+	@Override
+	public String toString() {
+		return "Data [numbers=" + Arrays.toString(numbers) + ", \nseparators=" + Arrays.toString(separators) + "]";
+	}
+
 }
--- src/main/java/dto/Line.java
+++ src/main/java/dto/Line.java
@@ -1,16 +1,26 @@
-﻿package dto;
+package dto;
 
 public class Line {
+
 	String custom;
 	String expression;
+
 	public Line(String custom, String expression) {
 		this.custom = custom;
 		this.expression = expression;
 	}
+
 	public String getCustom() {
 		return custom;
 	}
+
 	public String getExpression() {
 		return expression;
 	}
+
+	@Override
+	public String toString() {
+		return "Line [custom=" + custom + ", \nexpression=" + expression + "]";
+	}
+
 }
