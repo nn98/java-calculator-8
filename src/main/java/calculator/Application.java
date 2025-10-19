149e23d39b7f15d5a2b37ddf8127db7108e8e44f
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-19 14:09:24 +0900

    feat(app): 입출력 IO 클래스 적용 / 메인 프로그램 실행 흐름 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,15 +1,37 @@
 package calculator;
 
+import constants.Messages;
+import io.IO;
 import parser.Parser;
 import preprocessor.Preprocessor;
 
 public class Application {
+
+    public static final IO io = new IO();
+
     public static void main(String[] args) {
-        Preprocessor preprocessor = new Preprocessor("//;\n1;2;3;;;");
-        System.out.println(preprocessor.getProcessedLine().getCustom());
-        Parser parser = new Parser(preprocessor.getProcessedLine());
-        System.out.println(parser.getCalculateStack());
-        Calculator calculator = new Calculator(parser.getCalculateStack());
-        System.out.println(calculator.calculate());
+        String userInput = userInput();
+        int result = calculate(userInput);
+        printResult(result);
     }
+
+    private static String userInput() {
+        String userInput = io.readLine(Messages.notice);
+        if (userInput.length() > 2 && userInput.substring(0,2).equals("//"))
+            userInput += "\n" + io.readLine();
+        return userInput;
+    }
+
+    private static int calculate(String userInput) {
+        Preprocessor preprocessor = new Preprocessor(userInput);
+        Parser parser = new Parser(preprocessor.getProcessedLine());
+        Calculator calculator = new Calculator(parser.getCalculateStack());
+        int result = calculator.calculate();
+        return result;
+    }
+
+    private static void printResult(int result) {
+        io.writeLine(Messages.result + result);
+    }
+
 }
