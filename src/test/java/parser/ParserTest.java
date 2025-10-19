28c18238d90134322f773e170e95f5d09589ccee
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-19 15:26:15 +0900

    test(parser): 기능 이전에 따른 통합 파싱 테스트로 일원화

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -2,6 +2,8 @@ package parser;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Stack;
+
 import org.junit.jupiter.api.Test;
 
 import dto.Line;
@@ -9,15 +11,15 @@ import dto.Line;
 public class ParserTest {
 
 	@Test
-	void 계산식_숫자_추출_테스트() {
+	void 통합_파싱_테스트() {
 		Parser parser = new Parser(new Line(null,"1,2:3"));
-		assertThat(parser.getData().getNumbers()).isEqualTo(new String[]{"1", "2", "3"});
-	}
-
-	@Test
-	void 계산식_구분자_추출_테스트() {
-		Parser parser = new Parser(new Line(null,"1,2:3"));
-		assertThat(parser.getData().getSeparators()).isEqualTo(new String[]{",", ":"});
+		Stack<String> testStack = new Stack<>();
+		testStack.push("1");
+		testStack.push(",");
+		testStack.push("2");
+		testStack.push(":");
+		testStack.push("3");
+		assertThat(parser.getCalculateStack()).isEqualTo(testStack);
 	}
 
 }
