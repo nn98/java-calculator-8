7c9f01f1cd02845bc93cfb8b27c1ad053ee63ee5
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-19 15:34:20 +0900

    style(io): 컨벤션에 맞춰 개행 추가

--- src/main/java/io/IO.java
+++ src/main/java/io/IO.java
@@ -6,16 +6,20 @@ import java.io.PrintWriter;
 import camp.nextstep.edu.missionutils.Console;
 
 public class IO {
+
 	private final PrintWriter writer = new PrintWriter(new OutputStreamWriter(System.out), true);
 
 	public String readLine() {
 		return Console.readLine();
 	}
+
 	public String readLine(String prompt) {
 		writer.println(prompt);
 		return Console.readLine();
 	}
+
 	public void writeLine(String line) {
 		writer.println(line);
 	}
+
 }
