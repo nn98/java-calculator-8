c052aeb40b8fd6bb679300ab991d5dcb39a6c022
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-10-19 15:26:49 +0900

    test(preprocessor): 이전, 변경, 추가된 기능들에 대한 테스트 구현

--- src/test/java/preprocessor/PreprocessorTest.java
+++ src/test/java/preprocessor/PreprocessorTest.java
@@ -4,23 +4,32 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import dto.Line;
+
 public class PreprocessorTest {
 
 	@Test
+	void 통합_전처리_테스트() {
+		Preprocessor preprocessor = new Preprocessor("//!@!\\n1!@!3!@!5");
+		Line testLine = new Line("!@!", "1!@!3!@!5");
+		assertThat(preprocessor.getProcessedLine()).isEqualTo(testLine);
+	}
+
+	@Test
 	void 커스텀_구분자_추출_테스트() {
-		Preprocessor preprocessor = new Preprocessor("//;\n1;2;3");
+		Preprocessor preprocessor = new Preprocessor("//;\\n1;2;3");
 		assertThat(preprocessor.getProcessedLine().getCustom()).isEqualTo(";");
 	}
 
 	@Test
 	void 커스텀_구분자_추출_테스트_null() {
-		Preprocessor preprocessor = new Preprocessor("/;\n1;2;3");
+		Preprocessor preprocessor = new Preprocessor("1:2:3");
 		assertThat(preprocessor.getProcessedLine().getCustom()).isNull();
 	}
 
 	@Test
 	void 계산식_추출_테스트() {
-		Preprocessor preprocessor = new Preprocessor("//;\n1;2;3");
+		Preprocessor preprocessor = new Preprocessor("//;\\n1;2;3");
 		assertThat(preprocessor.getProcessedLine().getExpression()).isEqualTo("1;2;3");
 	}
 
@@ -28,6 +37,18 @@ public class PreprocessorTest {
 	void 계산식_공백_테스트() {
 		Preprocessor preprocessor = new Preprocessor("");
 		assertThat(preprocessor.getProcessedLine().getExpression()).isEqualTo("0");
+	}
+
+	@Test
+	void 커스텀_구분자_예외_테스트() {
+		assertThatThrownBy(() -> new Preprocessor("/;\\n1;2;3"))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	void 계산식_예외_테스트() {
+		assertThatThrownBy(() -> new Preprocessor("1;2;3a"))
+			.isInstanceOf(IllegalArgumentException.class);
 	}
 
 }
